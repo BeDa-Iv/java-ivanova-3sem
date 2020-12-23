5a50b2cf8aa6c1db91f082cb1d0a5f2d4737d7a7
Дарья <dashana2112@gmail.com>
2020-12-23 17:34:13 +0300

    Наследование с 1-5 задачи

--- src/Figures/Experiments.java
+++ src/Figures/Experiments.java
@@ -2,12 +2,12 @@ package Figures;
 
 public class Experiments {
     public static void main(String[] args) {
-        Circle a = new Circle(5);
-        Square b = new Square(5);
-        Rectangle c = new Rectangle(4, 6);
-        Rhombus d = new Rhombus(16, 9);
-        EquilateralTriangle e = new EquilateralTriangle(4);
-        RectangularTriangle f = new RectangularTriangle(4, 5);
+        Circle a = new Circle(3);
+        Square b = new Square(4);
+        Rectangle c = new Rectangle(5, 6);
+        Rhombus d = new Rhombus(10, 12);
+        EquilateralTriangle e = new EquilateralTriangle(7);
+        RectangularTriangle f = new RectangularTriangle(2, 8);
 
         Figure [] figures = {a, b, c, d, e, f};
         for (Figure i: figures) {
@@ -15,6 +15,7 @@ public class Experiments {
             System.out.println("Площадь: " + i.area());
             System.out.println("Периметр: " + i.perimeter());
             System.out.println("Ёмкость: " + i.capacity());
+            System.out.println("Ёмкость 2: " +  Figure.capacity(i));
             System.out.println("------------------------------");
         }
 
